6a00fc649f1fffca8e42342c9f8d086f256754e1
Bunyad Ahmadli <34852029+bunyadahmadli@users.noreply.github.com>
2020-11-16 14:04:51 +0300

    first commit

---
+++ UdemyNlayerProject.Core/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace UdemyNlayerProject.Core.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StockCode { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
