650fb31dc08462a6e9318033c219a6b76ad955a8
Bunyad Ahmadli <34852029+bunyadahmadli@users.noreply.github.com>
2020-11-19 23:14:50 +0300

    added IProductRepsository

---
+++ UdemyNlayerProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNlayerProject.Core.Models;
+
+namespace UdemyNlayerProject.Core.Repositories
+{
+    interface ICategoryRepository:IRepository<Category>
+    {
+        Task<Category> GetWithProductByIdAsync(int categoryId);
+
+    }
+}
---
+++ UdemyNlayerProject.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNlayerProject.Core.Models;
+
+namespace UdemyNlayerProject.Core.Repositories
+{
+    interface IProductRepository
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId)
+    }
+}
