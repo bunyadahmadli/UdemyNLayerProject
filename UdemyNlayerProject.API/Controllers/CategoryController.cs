c0b5fa896e871a4d792221985112eb473d841d4a
Bunyad Ahmadli <34852029+bunyadahmadli@users.noreply.github.com>
2020-11-24 22:48:10 +0300

    added categorydto and productdto

--- UdemyNlayerProject.API/Controllers/CategoryController.cs
+++ UdemyNlayerProject.API/Controllers/CategoryController.cs
@@ -1,12 +1,9 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UdemyNlayerProject.API.DTOs;
+using UdemyNlayerProject.Core.Models;
 using UdemyNlayerProject.Core.Service;
 
 namespace UdemyNlayerProject.API.Controllers
@@ -28,8 +25,52 @@ namespace UdemyNlayerProject.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var categories =await _categoryService.GetAllAsync();
+            var categories = await _categoryService.GetAllAsync();
             return Ok(_mapper.Map<IEnumerable<CategoryDto>>(categories));
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            return Ok(_mapper.Map<CategoryDto>(category));
+        }
+
+        [HttpGet("{id}/products")]
+        public async Task<IActionResult> GetWithProductsById(int id)
+        {
+            var category = await _categoryService.GetWithProductByIdAsync(id);
+
+            return Ok(_mapper.Map<CategoryWithProductDto>(category));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryDto categoryDto)
+        {
+           var newCategory= await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+
+            return Created(string.Empty,_mapper.Map<CategoryDto>(newCategory));
+        }
+
+        [HttpPut]
+        public IActionResult Update(CategoryDto categoryDto)
+        {
+            var category = _categoryService.Update(_mapper.Map<Category>(categoryDto));
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var category = _categoryService.GetByIdAsync(id).Result;
+            _categoryService.Remove(category);
+
+            return NoContent();
+        }
+
+       
+
     }
 }
