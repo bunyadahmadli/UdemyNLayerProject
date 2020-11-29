3fb98d8a63f2e00864d2c2990f7de485f01d8d7b
Bunyad Ahmadli <34852029+bunyadahmadli@users.noreply.github.com>
2020-11-29 21:50:03 +0300

    added categoryController/delete

--- UdemyNlayerProject.Web/Controllers/CategoriesController.cs
+++ UdemyNlayerProject.Web/Controllers/CategoriesController.cs
@@ -52,5 +52,13 @@ namespace UdemyNlayerProject.Web.Controllers
             _categoryService.Update(_mapper.Map<Category>(categoryDto));
             return RedirectToAction("Index");
         }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _categoryService.GetByIdAsync(id).Result;
+            _categoryService.Remove(category);
+            return RedirectToAction("Index");
+
+        }
     }
 }
